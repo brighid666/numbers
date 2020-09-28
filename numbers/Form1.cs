629bc40092c2d3af48d651ef5988c9bd3da84863
Brighid Ball <rc0040208@student.riversidecollege.ac.uk>
2020-09-28 15:56:41 +0100

    bean

--- numbers/Form1.cs
+++ numbers/Form1.cs
@@ -20,20 +20,44 @@ namespace numbers
         private void Form1_Load(object sender, EventArgs e)
         {
             double d1 = 5e6;
-           richTextBox1.AppendText(d1.ToString() + "\n");
-           richTextBox1.AppendText(d1.ToString("N") + "\n");
-           richTextBox1.AppendText(d1.ToString("N3") + "\n");
+            richTextBox1.AppendText(d1.ToString() + "\n");
+            richTextBox1.AppendText(d1.ToString("N") + "\n");
+            richTextBox1.AppendText(d1.ToString("N3") + "\n");
             richTextBox1.AppendText(d1.ToString("N0") + "\n");
             richTextBox1.AppendText(d1.ToString("0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+            string st = "three";
+            try
+            {
+                int it = int.Parse(st);
+            }
+            catch
+            {
+                MessageBox.Show(st + "is not an integer");
+            }
+            string s2 = "3.2";
+            try
+            {
+                int i2 = int.Parse(s2);
+            }
+            catch
+            {
+                MessageBox.Show(s2 + "is not an integer");
+                int i2 0;
+            }
+               string s3 = "pi";
+            try
+            {
+                double d2 = double.Parse(s3);
+            }
+                catch
+            {
+                double d2 = e;
+            }
 
+            int i1 = 3 * 4;
+            int i2
 
-
-
-
-        }
-        
-    }
 }
